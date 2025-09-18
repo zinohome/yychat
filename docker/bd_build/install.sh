29fce9c54d7005192898632bf08c255a6d3e7793
zhangjun <ibmzhangjun@139.com>
2025-09-18 11:50:31 +0800

    init

--- docker/bd_build/install.sh
+++ docker/bd_build/install.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 apt clean && rm -rf /var/lib/apt/lists/* && \
 rm /etc/apt/sources.list.d/passenger.list  && \
-apt-get --allow-insecure-repositories update && DEBIAN_FRONTEND=noninteractive && \
-apt install --allow-insecure-repositories -y --no-install-recommends build-essential libssl-dev libffi-dev python3-dev net-tools libsasl2-dev curl wget procps git libnss3-tools python3-pip && \
-apt install --allow-insecure-repositories -y software-properties-common  && add-apt-repository -y ppa:deadsnakes/ppa && apt install -y python3.11 && \
+apt-get update && DEBIAN_FRONTEND=noninteractive && \
+apt install -y --no-install-recommends build-essential libssl-dev libffi-dev python3-dev net-tools libsasl2-dev curl wget procps git libnss3-tools python3-pip && \
+apt install -y software-properties-common  && add-apt-repository -y ppa:deadsnakes/ppa && apt install -y python3.11 && \
 rm /usr/bin/python && ln -s /usr/bin/python3.11 /usr/bin/python && \
 python -m pip install virtualenv && \
 cd /opt && \
